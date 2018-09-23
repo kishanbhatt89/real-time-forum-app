dda80a9e7b0ff85e503453cbd56c4f079ac70bba
Kishan Bhatt <35603034+kishanubhatt@users.noreply.github.com>
2018-09-23 19:15:43 +0530

    Working login component

--- resources/assets/js/components/login/Login.vue
+++ resources/assets/js/components/login/Login.vue
@@ -1,10 +1,38 @@
 <template>
-    <h1>Login</h1>
+
+    <v-container>
+
+        <v-form @submit.prevent="login">
+
+            <v-text-field type="email" v-model="form.email" label="E-mail" required></v-text-field>
+
+            <v-text-field type="password" v-model="form.password" label="Password" required></v-text-field>
+
+            <v-btn color="green" type="submit">Login</v-btn>
+
+        </v-form>
+
+  </v-container>
+
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            form:{
+                email: null,
+                password: null
+            }
+        }
+    },
+    methods:{
+        login(){
+            axios.post('/api/auth/login',this.form)
+                 .then(res => console.log(res.data))
+                 .catch(error => console.log(error.res.data))
+        }
+    }
 }
 </script>
 
